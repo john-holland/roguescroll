0cb0db2e80bd7cceb4edf5423f4b3d2b3075d2d5
John Holland <john.gebhard.holland@gmail.com>
2025-06-18 17:34:13 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 var path = require('path');
 
 module.exports = {
+    mode: 'development',
     context: __dirname + "/client/js",
     entry: path.resolve(__dirname, "client/js/context.js"),
     output: {
         path: path.resolve(__dirname, "client/"),
         filename: "game.js"
     },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
      module: {
-        loaders: [
-            { test: /\.hbs$/i, loader: "handlebars-loader" },
-            { test: /\.html$|\.css$/i, loader: "raw" },
-            { test: /\.html$/i, loader: "html" },
-            { test: /\.css$/i, loader: "css" }
+        rules: [
+            { test: /\.hbs$/i, use: "handlebars-loader" },
+            { test: /\.html$|\.css$/i, use: "raw-loader" },
+            { test: /\.html$/i, use: "html-loader" },
+            { test: /\.css$/i, use: "css-loader" }
         ]
     }
 };
